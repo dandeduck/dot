fb1e524c5d643a76538ce643275a3c00e1de40f7
dandeduck <danielmkhlv@gmail.com>
2020-10-03 19:25:12 +0300

    changed how permissions work

--- app/src/main/java/com/dot/maps/BasicPlaceFactory.java
+++ app/src/main/java/com/dot/maps/BasicPlaceFactory.java
@@ -1,5 +1,6 @@
 package com.dot.maps;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -17,27 +18,33 @@ public class BasicPlaceFactory extends PlaceFactory {
     private static final int MAX_RESULTS = 20;
     private static final double SEARCH_RADIUS = 50000;
     private final AtomicReference<LatLng> currentLocation;
+    private final Activity activity;
 
     public BasicPlaceFactory(Activity activity, String apiKey) {
         super(activity.getApplicationContext(), apiKey, MAX_RESULTS, SEARCH_RADIUS);
 
+        this.activity = activity;
         currentLocation = new AtomicReference<>();
-        setupLocationManager(activity);
+
+        setupLocationManager();
     }
 
     public void request(String userInput, Callback<List<Place>> callback) {
-        super.request(currentLocation.get(), userInput, callback);
+        if (missingPermission())
+            setupLocationManager();
+        else
+            super.request(currentLocation.get(), userInput, callback);
     }
 
-    private void setupLocationManager(Activity activity) {
-        try {
-            if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-                ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+    @SuppressLint("MissingPermission")
+    private void setupLocationManager() {
+        if (missingPermission())
+            ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+        else
+            ((LocationManager)activity.getSystemService(Context.LOCATION_SERVICE)).requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location -> currentLocation.set(new LatLng(location.getLatitude(), location.getLongitude())));
+    }
 
-        ((LocationManager)activity.getSystemService(Context.LOCATION_SERVICE)).requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location -> currentLocation.set(new LatLng(location.getLatitude(), location.getLongitude())));
+    private boolean missingPermission() {
+        return ContextCompat.checkSelfPermission(activity.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED;
     }
 }
